e035925329a6e3749244f2a66f7c514aa15c5dc0
John Phillip <jpbebora@gmail.com>
2023-03-10 15:13:07 +0800

    Initial Abstract User models

---
+++ app_user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+class AdminUser(AbstractUser):
+    email = models.EmailField(unique=True, null=False)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username',
+        'first_name',
+        'last_name',
+        ]
+
+    def __str__(self):
+        return self.last_name
