d80bd39a51b13d0b606b3ed5d32451c61900f214
John Phillip <jpbebora@gmail.com>
2023-03-15 14:46:50 +0800

    Clean some endpoints

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     # blog-deleted
-    path('blog-deleted/', views.BlogDeletedListView.as_view(), name='blog-list-deleted'),
-    path('blog-deleted/<str:pk>', views.BlogRestoreView.as_view(), name='blog-restore-deleted'),
+    path('deleted/', views.BlogDeletedListView.as_view(), name='blog-list-deleted'),
+    path('deleted/<str:pk>', views.BlogRestoreView.as_view(), name='blog-restore-deleted'),
     # blog
-    path('blog/', views.BlogListView.as_view(), name='blog-list'),
-    path('blog/<str:title>', views.BlogDeleteView.as_view(), name='blog-detail'),
+    path('', views.BlogListView.as_view(), name='blog-list'),
+    path('<str:title>', views.BlogDeleteView.as_view(), name='blog-detail'),
 ]
--- phil_explorer/urls.py
+++ phil_explorer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app_blog.urls')),
     path('api/', include('app_user.urls')),
+    path('api/blog/', include('app_blog.urls')),
     path('api/blog/search/', include('blog_search.urls')),
 ]
