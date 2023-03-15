03dd2cb216f01c41c0f1bf6e7c8314ce24e472b1
John Phillip <jpbebora@gmail.com>
2023-03-15 09:02:02 +0800

    modify blog endpoints

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # blog-deleted
     path('blog-deleted/', views.BlogDeletedListView.as_view(), name='blog-list-deleted'),
     path('blog-deleted/<str:pk>', views.BlogRestoreView.as_view(), name='blog-restore-deleted'),
+    # blog
     path('blog/', views.BlogListView.as_view(), name='blog-list'),
     path('blog/<str:pk>', views.BlogDeleteView.as_view(), name='blog-delete'),
 ]
