d43f1768c3884bb95b35968d35dcd1378d253489
John Phillip <jpbebora@gmail.com>
2023-03-20 10:31:21 +0800

    added authorization

--- blog_search/views.py
+++ blog_search/views.py
@@ -1,22 +1,31 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from app_blog.models import Blog, BlogManager
 from app_blog.serializers import BlogSerializer
+
+class IsAuthenticatedOrQueryParamsAllowed(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        else:
+            return request.GET.get('filter') is None
 
 class SearchListBlog(generics.ListAPIView):
     queryset = Blog.objects.filter(is_deleted=False)
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrQueryParamsAllowed]
 
     def get_queryset(self, *args, **kwargs):
-        qs = super().get_queryset(*args, **kwargs)
+        qs = BlogManager(Blog)
         q = self.request.GET.get('q')
         date_by = self.request.GET.get('filter')
-        result = qs
         if q is not None:
-            result = BlogManager(Blog).search(q)
-        if date_by == 'latest':
-            result = result.order_by('-date_created')
-        elif date_by == 'oldest':
-            result = result.order_by('date_created')
+            result = qs.search(q)
         else:
-            pass
+            result = Blog.objects.none()
+
+        if self.request.user.is_authenticated and date_by is not None:
+            if date_by == 'latest':
+                result = result.order_by('-date_created')
+            elif date_by == 'oldest':
+                result = result.order_by('date_created')
         return result
