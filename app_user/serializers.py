149e1e7c769b779b280048d180afb160bc0177dd
John Phillip <jpbebora@gmail.com>
2023-03-20 16:30:35 +0800

    added confirm password

--- app_user/serializers.py
+++ app_user/serializers.py
@@ -5,30 +5,44 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 
 
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = AdminUser
-        fields = ['email', 'password', 'first_name', 'last_name', 'date_created', 'date_updated']
+        fields = [
+            'email',
+            'password',
+            'password2',
+            'first_name',
+            'last_name',
+            'date_created',
+            'date_updated',
+        ]
 
-    # check if the user meet the password complexity requirement
-    def validate_password(self, value):
-        validate_password(value)
-        return value
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'password not match.'})
+        return attrs
+
     def create(self, validated_data):
         user = AdminUser.objects.create_user(
             username=validated_data['email'],
             email=validated_data['email'],
-            password=validated_data['password'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name']
         )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
     def validate(self, data):
         email = data.get('email')
         password = data.get('password')
@@ -40,9 +54,3 @@ class LoginSerializer(serializers.Serializer):
         else:
             raise serializers.ValidationError('Invalid login credentials')
         return data
-
-
-
-
-
-
