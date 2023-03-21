f9120edd3e5c3d7e62eac937f75d174d68626889
John Phillip <jpbebora@gmail.com>
2023-03-21 19:04:44 +0800

    Flexible Search

--- blog_search/views.py
+++ blog_search/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from app_blog.models import Blog, BlogManager
 from app_blog.serializers import BlogSerializer
 
+
 class IsAuthenticatedOrQueryParamsAllowed(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
@@ -14,12 +15,23 @@ class SearchListBlog(generics.ListAPIView):
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticatedOrQueryParamsAllowed]
 
-    def get_queryset(self, *args, **kwargs):
+    def get_queryset(self, result=None, *args, **kwargs):
         qs = BlogManager(Blog)
         q = self.request.GET.get('q')
         date_by = self.request.GET.get('filter')
         if q is not None:
             result = qs.search(q)
+        elif self.request.user.is_authenticated and date_by is not None:
+            if date_by == 'latest':
+                if result is not None:
+                    result = result.order_by('-date_created')
+                else:
+                    result = qs.filter(is_deleted=False).order_by('date_created')
+            elif date_by == 'oldest':
+                if result is not None:
+                    result = result.order_by('date_created')
+                else:
+                    result = qs.filter(is_deleted=False).order_by('date_created')
         else:
             result = Blog.objects.none()
 
