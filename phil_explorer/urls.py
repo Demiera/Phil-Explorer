f6be835200524b2e8e787c83d111f78f036f638f
John Phillip <jpbebora@gmail.com>
2023-04-05 10:20:04 +0800

    add URL to static image root

--- phil_explorer/urls.py
+++ phil_explorer/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('api/blog/', include('app_blog.urls')),
     path('api/blog/search/', include('blog_search.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
