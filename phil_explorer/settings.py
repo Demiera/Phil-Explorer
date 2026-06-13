2712478fc5fca2cdb189b41f38a9e0b46ad3f533
John Phillip C. Bebora <91673817+Demiera@users.noreply.github.com>
2026-06-13 18:58:32 +0800

    error

--- phil_explorer/settings.py
+++ phil_explorer/settings.py
@@ -79,7 +79,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
