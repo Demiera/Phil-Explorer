5fe580eb25922a12699f450fea065923ad853cc4
John Phillip C. Bebora <91673817+Demiera@users.noreply.github.com>
2026-06-13 11:52:30 +0800

    version 1.1

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing'),
-    path('blogs/', views.blog_list, name='blog-list-page'),
-    path('blogs/<slug:slug>/', views.blog_detail, name='blog-detail-page'),
+    path('', views.dashboard_published, name='dashboard'),
+    path('drafts/', views.dashboard_drafts, name='drafts'),
+    path('deleted/', views.dashboard_deleted, name='deleted'),
+    path('create/', views.create_post, name='create-post'),
+    path('edit/<slug:slug>/', views.edit_post, name='edit-post'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.login_view, name='login-page'),
     path('register/', views.register_view, name='register-page'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
-
-def landing(request):
-    return render(request, 'frontend/landing.html')
-
-def blog_list(request):
-    return render(request, 'frontend/blog_list.html')
-
-def blog_detail(request, slug):
-    return render(request, 'frontend/blog_detail.html', {'slug': slug})
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
-    return render(request, 'frontend/app_user/login.html')
+    return render(request, 'frontend/login.html')
 
 def register_view(request):
     return render(request, 'frontend/register.html')
+
+def dashboard_published(request):
+    return render(request, 'frontend/dashboard_published.html')
+
+def dashboard_drafts(request):
+    return render(request, 'frontend/dashboard_drafts.html')
+
+def dashboard_deleted(request):
+    return render(request, 'frontend/dashboard_deleted.html')
+
+def create_post(request):
+    return render(request, 'frontend/create_post.html')
+
+def edit_post(request, slug):
+    return render(request, 'frontend/edit_post.html', {'slug': slug})
+
+def profile(request):
+    return render(request, 'frontend/profile.html')
