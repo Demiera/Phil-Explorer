efc9b893dc89ff55cd17e890682f9155eb0262d7
John Phillip C. Bebora <91673817+Demiera@users.noreply.github.com>
2026-06-18 11:58:18 +0800

    edited

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.public_blog_list, name='public-blog-list'),
-    path('<slug:slug>/', views.public_blog_detail, name='public-blog-detail'),
     path('dashboard/', views.dashboard_published, name='dashboard'),
     path('drafts/', views.dashboard_drafts, name='drafts'),
     path('deleted/', views.dashboard_deleted, name='deleted'),
@@ -12,4 +11,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', views.login_view, name='login-page'),
     path('register/', views.register_view, name='register-page'),
+
+    path('<slug:slug>/', views.public_blog_detail, name='public-blog-detail'),
+
 ]
